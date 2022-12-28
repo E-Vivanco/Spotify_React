58ecee4a5879b9cd24e2392893dbf6048e038294
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-28 05:18:42 +0000

    act spotify-react

--- spotify_react/src/App.js
+++ spotify_react/src/App.js
@@ -1,181 +1,138 @@
 
-import { useRef, useState,useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './styles.css'
 
 function App () {
 
-  const [song, setSong] = useState([
-    { "id":1, "category":"game", "name":"Mario Castle", "url":"files/mario/songs/castle.mp3" },
-    { "id":2, "category":"game", "name":"Mario Star", "url":"files/mario/songs/hurry-starman.mp3"},
-    { "id":3, "category":"game", "name":"Mario Overworld", "url":"files/mario/songs/overworld.mp3"},
-    ])
-let a,b
-    console.log("soy todas las canciones",song)
-  const [pos, setPos] = useState(0);
-  console.log("soy pos:", pos)
-  const [audio] = useState( 
-    typeof Audio !== "undefined" && new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"),
-    typeof Audio !== "undefined" && new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/hurry-starman.mp3"),
-    typeof Audio !== "undefined" && new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/overworld.mp3"),
-  )
-  //typeof Audio !== "undefined" && new Audio("songURL+song[pos].url")); 
-  //this will prevent rendering errors on NextJS since NodeJs doesn't recognise HTML tags neither its libs.
-  const [isPlaying, setIsPlaying] = useState(false);
-  //console.log("soy la cancion:",song[0].url)
-  const songURL = "https://assets.breatheco.de/apis/sound/";
-  let audioRef = useRef();
-  let botonizRef = useRef();
-  let botonderRef = useRef();
-  let botoncenRef = useRef();
-  console.log("soy audioref", audioRef)
-  console.log("soy boton iz",botonizRef)
-  console.log("soy boton der",botonderRef)
-  console.log("soy boton cen",botoncenRef)
-  
-  const selectAudio = (audio,pos) => {
-    if(audioRef.current)
-    {audioRef.current.src= audio.url
-     //audio.url = true
-    setPos(pos)}
-  
-  }
-  useEffect(() => {
-    isPlaying ? audio.play() : audio.pause();
-  }, [isPlaying]);
- // const [play,setPlay] = useState(false)
+  const [songs, setSongs] = useState([]) // arreglo vacio para contener canciones x medio de la consulta fetch
+  const [isPlaying, setPlaying] = useState(false); // setemos en estado false el play
+  const [isLoading, setIsLoading] = useState(true); // indicador de carga de consulta fetch
+  const [activaSong,setactivaSong] =useState(''); // controolamos cada cancion que se activa
  
-  
-//function playS(a=1){
-//  setPlay(
-//  {play:true},
-//  {pause:false}
-//  )
-//}
-//
-//function pauseS(b=0){
-//  setPlay(
-//    {pause: true},
-//    {play: false}
-//  )
-//}
+  const songURL = "https://assets.breatheco.de/apis/sound/";
+  let audioRef = useRef(); //sacamos referencia del audio
+  let posRef = useRef();// sacamos refenecia de la posicion
 
+  const avanzaBoton = (avanza) => {  // funcion que nos permite avanzar con boton de play
+    if (audioRef.current) {
+      audioRef.current.pause();
+    }
+  
+    if (avanza === 'backward') {
+      if (posRef.current > 0) {
+        posRef.current--;
+      } else {
+        posRef.current = songs.length - 1;
+      }
+    } else if (avanza === 'forward') {
+      if (posRef.current < songs.length - 1) {
+        posRef.current++;
+      } else {
+        posRef.current = 0;
+      }
+    }
+  
+    const activaSong = songs[posRef.current];//asignamos una posicion a la cancion
+   //console.log(activaSong)
+    const allUrl = songURL + activaSong.url; //enviamos toda la url de play
+    //console.log(allUrl)
+    const otroAudio = new Audio(allUrl);  //creamos nuevo audio
+    //console.log(otroAudio)
+    otroAudio.play(); //creamos nuevo audio
+    //console.log(otroAudio.play())
+    audioRef.current = otroAudio; //pasamos referencia a nuevo audio 
+   // console.log("soy todas las canc:",songs)
+    setactivaSong(activaSong.id+' '+activaSong.name);
+ //   console.log("lista de lector",setactivaSong(activaSong.id+' '+activaSong.name))
+    setPlaying(true);//estado de play activado
+  };
+  
+  const PlayBoton = () => {   // funcion que controla la activacion o detencion de cancion seleccionada
+    if (audioRef.current) {
+      if (audioRef.current.paused) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+    setPlaying(!isPlaying);
+  };
+  
+
+const cancionClick = (name, url) => {  // rescatamos de la refencia los datos de la cancion mas la url para pintar tabla y
+  if (audioRef.current) {         // se activa cada cancion de la tabla al hacer click
+    audioRef.current.pause();
+  }
+
+  const allUrl = songURL + url;
+  //console.log(allUrl)
+  const otroAudio = new Audio(allUrl);
+  //console.log(otroAudio)
+  otroAudio.play();
+  //console.log(otroAudio.play())
+  audioRef.current = otroAudio;
+  setactivaSong(name); // capturamos el nombe de la cancion activada
+  //console.log(setactivaSong(name))
+  setPlaying(true);
+  posRef.current = songs.findIndex((song) => song.url === url);
+  //console.log(posRef.current)
+};
+
+useEffect(() => {     //hooks que realiza la consulta fetch y setea la data en setSongs
+  const recuperaSongs = async ()=>{
+   try{
+     const response =  await fetch("https://assets.breatheco.de/apis/sound/songs");
+     const info2= await response.json();
+           setSongs(info2);
+           setIsLoading(false);
+       }catch(error){
+         console.log(error);
+       }       
+     }
+     recuperaSongs()
+     }, []);
+if (isLoading) {<h1>Cargando...</h1> }  // bandera de carga mientras actua fetch
+ // se pinta la tabla de canciones con formato previa mente definido en style junto con los botones
   return (
     <>
       <div className='titulo'><h2 className='my-2 py-2'>Spotify-React</h2></div>
       <div className=" card d-flex w-100 flex-column">
-        {
-          song.length > 0 ?
-          song.map((aud, index1) => {
-            return (
-              <div className="songs my-1 w-100" key={index1}>
-                <audio  controls>
-        
-                <source className="base w-100" src={ songURL + song[index1].url} ref={audioRef} type="audio/mpeg" onClick={() => selectAudio(aud, pos)}/>         
-                 </audio>
-                 
-              </div>
-              
-            )
-           
-          })
-          : song
-        }
+              <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Titulo</th>
+                            <th>URL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {songs.map((song) => (
+                            <tr className="song" key={song.url} onClick={() => cancionClick(song.name, song.url)}>
+                                <td>{song.id}</td>
+                                <td>{song.name}</td>
+                                <td>{song.url}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                </div>
+          
       </div> 
-      <div className='botones d-flex flex-row'> 
-      <source ref={audioRef} src={songURL + song[pos].url} type="audio/mpeg" onClick={() => audio.play()}/> 
-      <button  ref={botonizRef} className=" bot_izq my-1 inv fa-solid fa-play" src={songURL + song[pos].url} onClick={() => setPos(pos => pos === 0 ? song.length - 1 : pos - 1)+ audio.play()}></button>
-      <button ref={botoncenRef}src={songURL + song[pos].url} onClick={() => setPos(pos => pos === song.length - 1 ? 0 : pos + 1)+ audio.pause()} className={"bot_der my-1 der fa-solid fa-play fa-2x " +('click' ? "bot_paus my-1 fa-solid fa-pause fa-1x" : null  )}>
-      </button> 
-      <button ref={botonderRef} className=" bot_der my-1 der fa-solid fa-play"src={songURL + song[pos].url} onClick={() => setPos(pos => pos === song.length - 1 ? 0 : pos + 1)+ audio.play()}></button> 
-       </div>     
+      
+      <div className="botones d-flex flex-row'">
+      
+            <button className="btn btn-dark m-2" onClick={() => avanzaBoton('backward')}><i className="fa-solid fa-backward"></i></button>
+            <button className="btn btn-dark m-2" onClick={PlayBoton}>{isPlaying ? <i className="fa-solid fa-pause"></i> : <i className="fa-solid fa-play"></i>}</button>
+            <button className="btn btn-dark m-2" onClick={() => avanzaBoton('forward')}><i className="fa-solid fa-forward"></i></button>
+            <div className="song m-4">{activaSong}</div>
+  
+        </div>
+             
+           
     </>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import { useRef, useState ,useEffect} from 'react';
-import './styles.css'
-
-function App() {
-
-  const [song, setSong] = useState([
-    { "id":1, "category":"game", "name":"Mario Castle", "url":"files/mario/songs/castle.mp3" },
-    { "id":2, "category":"game", "name":"Mario Star", "url":"files/mario/songs/hurry-starman.mp3"},
-    { "id":3, "category":"game", "name":"Mario Overworld", "url":"files/mario/songs/overworld.mp3"},
-    ])
-  const [pos, setPos] = useState(0);
-  //console.log("soy la cancion:",song[0].url)
-  const songURL = "https://assets.breatheco.de/apis/sound/";
-  let audioRef = useRef();
-  const selectAudio = (audio,pos) => {
-    audioRef.current.src = audio.url;
-    setPos(pos)
-   // audioRef.current.name= audio;
-   // audioRef.current.url= audio
-  }
- /* useEffect(() => {
-      audioRef.current.type.name='Leopardo';
-    
-  }, [])*/
- /* const captValor = () => {
-    console.log(audioRef.current.type.value);
-    return (audioRef.current.type.value);
-  }
-  return (
-    <>
-      <div className='titulo'><h2 className='my-2 py-2'>Spotify-React</h2></div>
-      <div className=" card d-flex w-100 flex-column">
-        {
-          song.length > 0 ?
-          song.map((aud, index) => {
-            return (
-              <div className="songs my-1 w-100" key={index}>
-                <audio  controls>
-        
-                <source className="base w-100" src={aud.url} ref={audioRef} type="audio/mpeg" onClick={() => selectAudio(aud, index)}/>
-                <source ref={audioRef} src={songURL + song[pos].url}/>
-                
-                 </audio>
-                 
-              </div>
-              
-            )
-           
-          })
-          : song
-        }
-      </div> 
-      <div className='botones d-flex flex-row'>
-      <button className=" bot_izq my-1 inv fa-solid fa-play" ref={audioRef} onClick={() => selectAudio(songURL, song[setPos(pos => pos === 0 ? song.length - 1 : pos - 1)].url)}></button>
-      <button className=" bot_paus my-1 fa-solid fa-pause" ref={audioRef} onClick={() => selectAudio( songURL, song[setPos(pos => pos === song.length - 1 ? 0 : pos + 1)].url)}></button> 
-      <button className=" bot_der my-1 der fa-solid fa-play" ref={audioRef} onClick={() => selectAudio( songURL, song[setPos(pos => pos === song.length - 1 ? 0 : pos + 1).url])}></button> 
-      </div>     
-    </>
-  );
-}
-
-export default App;*/
 
